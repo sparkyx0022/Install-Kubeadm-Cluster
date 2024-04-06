a37736bccb0f6f1d0ffb4a30035de371043520e5
Rohan Shinde <37873955+sparkyx0022@users.noreply.github.com>
2024-04-06 11:12:57 +0530

    Update worker.sh

--- worker.sh
+++ worker.sh
@@ -77,10 +77,11 @@ sudo systemctl restart docker
 ### UPDATING AND ADDING KEYRING OF K8s #### 
 sudo apt-get update
 sudo apt-get install -y apt-transport-https ca-certificates curl
-sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
-echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+mkdir /etc/apt/keyrings
 
-### INSTALLATING K8s #### 
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list### INSTALLATING K8s #### 
+
 sudo apt-get update -y
 sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
